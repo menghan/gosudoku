a46d7d8bbc59cf468b0b8b245bcfa3fed7042d8f
Menghan <menghan412@gmail.com>
2013-08-21 18:43:04 +0800

    make a stack, still error

--- solver.go
+++ solver.go
@@ -1,11 +1,14 @@
 package main
 
-import "os"
-import "fmt"
-import "strconv"
+import (
+	"os"
+	"fmt"
+	"strconv"
+	list "container/list"
+)
 
 type Puzzle struct {
-	grid [9][9]uint8
+	grid       [9][9]uint8
 	candidates [9][9]uint16
 }
 
@@ -17,7 +20,7 @@ func (puzzle *Puzzle) InitFromFile(filename string) {
 	}
 	defer input_file.Close()
 	buf := make([]byte, 10)
-	for x := 0; x < 9; x ++ {
+	for x := 0; x < 9; x++ {
 		n, _ := input_file.Read(buf)
 		if n == 0 {
 			break
@@ -45,8 +48,40 @@ func (puzzle *Puzzle) Print() {
 	}
 }
 
+type Stack struct {
+	count uint
+	nodes list.List
+}
+
+func (stack *Stack) Init() {
+	stack.nodes.Init()
+}
+
+func (stack *Stack) Push(item Puzzle) {
+	stack.nodes.PushBack(item)
+	stack.count++
+}
+
+func (stack *Stack) Pop() Puzzle {
+	stack.count--
+	poped := stack.nodes.Back()
+	value := stack.nodes.Remove(poped)
+	return value
+}
+
+func resolve(puzzle Puzzle) Puzzle {
+	var stack Stack
+	stack.Init()
+	stack.Push(puzzle)
+	result := stack.Pop()
+	return result
+}
+
 func main() {
 	var puzzle Puzzle
 	puzzle.InitFromFile("puzzle1")
 	puzzle.Print()
+	result := resolve(puzzle)
+	fmt.Println("result")
+	result.Print()
 }
