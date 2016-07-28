7f384f1c76d728d836fe743fb287af74b29095fe
Menghan <menghan412@gmail.com>
2016-07-28 09:22:47 +0800

    add big solve test case

--- solver_test.go
+++ solver_test.go
@@ -6,23 +6,46 @@ import (
 	"testing"
 )
 
-func benchmarkSolver(b *testing.B, puzzleFilename string, concurrency int) {
+func benchmarkSolver(b *testing.B, puzzleFilename string, expectResultCount int, concurrency int) {
 	var puzzle Puzzle
 	puzzle.MustLoadFromFile(puzzleFilename)
 	solver := newSolver(concurrency)
 	for n := 0; n < b.N; n++ {
 		solver.Solve(&puzzle)
 	}
+	if len(solver.results) != expectResultCount {
+		b.Fatalf("Wrong result count, expect %d, got %d", expectResultCount, len(solver.results))
+	}
 }
 
-func BenchmarkSolver1(b *testing.B) {
-	benchmarkSolver(b, "puzzle4", 1)
+func benchmarkSmallSolver(b *testing.B, concurrency int) {
+	benchmarkSolver(b, "puzzle4", 1, concurrency)
 }
 
-func BenchmarkSolver2(b *testing.B) {
-	benchmarkSolver(b, "puzzle4", 2)
+func benchmarkBigSolver(b *testing.B, concurrency int) {
+	benchmarkSolver(b, "puzzle6", 5122, concurrency)
 }
 
-func BenchmarkSolver4(b *testing.B) {
-	benchmarkSolver(b, "puzzle4", 4)
+func BenchmarkSmallSolver1(b *testing.B) {
+	benchmarkSmallSolver(b, 1)
+}
+
+func BenchmarkSmallSolver2(b *testing.B) {
+	benchmarkSmallSolver(b, 2)
+}
+
+func BenchmarkSmallSolver4(b *testing.B) {
+	benchmarkSmallSolver(b, 4)
+}
+
+func BenchmarkBigSolver1(b *testing.B) {
+	benchmarkBigSolver(b, 1)
+}
+
+func BenchmarkBigSolver2(b *testing.B) {
+	benchmarkBigSolver(b, 2)
+}
+
+func BenchmarkBigSolver4(b *testing.B) {
+	benchmarkBigSolver(b, 4)
 }
